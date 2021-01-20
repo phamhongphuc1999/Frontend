011d3827a17774afb052746c58430773c2160a28
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2021-01-20 15:20:23 +0700

    fix: header in myfacebook and add: mywebsite

--- myfacebook/src/Components/Header/ListItemHeader.js
+++ myfacebook/src/Components/Header/ListItemHeader.js
@@ -4,7 +4,7 @@ class ListItemHeader extends Component{
     render() {
         return(
             <li className={this.props.itemClass}>
-                <span><i className={this.props.iconClass}></i></span>
+                <span><i className={this.props.iconClass} id={this.props.itemId}></i></span>
                 <div className="tooltip">{this.props.tipText}</div>
                 <div className="number-message">{this.props.number}</div>
             </li>
